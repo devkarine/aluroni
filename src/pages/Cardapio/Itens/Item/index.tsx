79471e857a8a6ba206167bbe971d05f985bc4379
Karine <karinedeveloper@gmail.com>
2023-12-05 19:37:22 -0300

    feat: adiciona categorias e imagens

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,6 +1,6 @@
-import logo from "assets/logo.svg";
 import styles from "./Item.module.scss";
 import cardapio from '../intens.json'
+import classNames from 'classnames';
 
 type Props = typeof cardapio[0]
 
@@ -17,7 +17,10 @@ export const Item = (props: Props) => {
           <p>{description}</p>
         </div>
         <div className={styles.item__tags}>
-          <div className={styles.item__tipo}>{category.label}</div>
+          <div className={classNames({
+            [styles.item__tipo]: true,
+            [styles[`item__tipo__${category.label.toLowerCase()}`]]: true
+          })}>{category.label}</div>
           <div className={styles.item__porcao}>{size}g</div>
           <div className={styles.item__qtdpessoas}>Serve {serving} pessoa{serving === 1 ? '' : 's'}</div>
           <div className={styles.item__valor}>R${price.toFixed(2)}</div>
