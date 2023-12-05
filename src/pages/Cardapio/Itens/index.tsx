ab77e51fc51a901cf0fbd415d16dae5112c700ee
Karine <karinedeveloper@gmail.com>
2023-12-05 20:03:18 -0300

    feat: adiciona busca e filtros

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -1,15 +1,40 @@
-import { Item } from './Item'
-import cardapio from './intens.json'
-import styles from './Itens.module.scss'
+import { Item } from "./Item";
+import cardapio from "./intens.json";
+import styles from "./Itens.module.scss";
+import { useEffect, useState } from "react";
 
-export const Itens = () =>{
-    return(
-        <div className={styles.itens}>
-            {cardapio.map((item) => (
-                <Item key={item.id}
-                {...item}
-                />
-            ))}
-        </div>
-    )
+interface Props {
+  busca: string;
+  filtro: number | null;
+  ordenador: string;
 }
+
+export const Itens = (props: Props) => {
+  const [lista, setLista] = useState(cardapio);
+  const { busca, filtro } = props;
+
+  const testaBusca = (title: string) => {
+    const regex = new RegExp(busca, "i");
+    return regex.test(title);
+  };
+
+  const testaFiltro = (id: number) => {
+    if (filtro !== null) return filtro === id;
+    return true;
+  };
+
+  useEffect(() => {
+    const novaLista = cardapio.filter(
+      (item) => testaBusca(item.title) && testaFiltro(item.category.id)
+    );
+    setLista(novaLista);
+  }, [busca, filtro]);
+
+  return (
+    <div className={styles.itens}>
+      {lista.map((item) => (
+        <Item key={item.id} {...item} />
+      ))}
+    </div>
+  );
+};
